c3a472eb25fae500a6116cd936f5175978463776
peipeipeipeigit <szupei21@gmail.com>
2023-12-17 18:18:46 +0800

    modify GET '/todos/:id' API

--- app.js
+++ app.js
@@ -40,7 +40,12 @@ app.get('/users/logout', (req, res) => {
 })
 
 // browse
-
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
 
 app.get('/', (req, res) => {
   return Todo.findAll({
