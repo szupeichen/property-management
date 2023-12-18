2e5c87f96e67a30ef89242df27a89b302d71523d
peipeipeipeigit <szupei21@gmail.com>
2023-12-19 02:34:38 +0800

    add passport.js and modified the login POST API

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
+// 載入passport設定檔，要寫在 express-session 以後
+const passport = require('passport')
+const usePassport = require('./config/passport')
 
 const app = express()
 const PORT = 3000
@@ -12,17 +16,29 @@ const User = db.User
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
 
 
 // login
 app.get('/users/login', (req, res) => {
   res.render('login')
 })
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+// login route 加入 middleware，驗證 reqest 登入狀態
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+
 
 // register
 app.get('/users/register', (req, res) => {
