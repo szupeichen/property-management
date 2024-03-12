09942c386f6fbb071c670f13d6cca4065b1b2a50
lydia chen <szupei21@gmail.com>
2024-03-13 02:50:10 +0800

    made association between model-Unit and model-Agency

--- models/unit.js
+++ models/unit.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate (models) {
       // define association here
+      Unit.belongsTo(models.Agency)
     }
   }
   Unit.init({
