338a8c1e86bf73dc56ac4c55cef9d13132e0ab41
lydia chen <szupei21@gmail.com>
2024-04-09 15:17:03 +0800

    add income and annualIncome column in index.hbs

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -2,5 +2,6 @@ const dayjs = require('dayjs') // 載入 dayjs 套件
 
 module.exports = {
   currentYear: () => dayjs().year(), // 取得當年年份作為 currentYear 的屬性值，並導出
-  formatDate: (date) => dayjs(date).format('YYYY/MM/DD')
+  formatDate: (date) => dayjs(date).format('YYYY/MM/DD'),
+  annualIncome: (income) => { return income * 12 }
 }
