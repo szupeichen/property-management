79f18adf5ed9175070f5f4845af8563fa09dcb59
Lydia Chen <56731175+peipeipeipeigit@users.noreply.github.com>
2024-06-24 18:44:44 +0800

    Update index.js

--- models/index.js
+++ models/index.js
@@ -9,12 +9,22 @@ const env = process.env.NODE_ENV || 'development'
 const config = require(path.resolve(__dirname, '../config/config.json'))[env]
 const db = {}
 
-let sequelize
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config)
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config)
-}
+// let sequelize
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config)
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config)
+// }
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT
+  }
+)
 
 fs
   .readdirSync(__dirname)
