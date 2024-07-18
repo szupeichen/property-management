fadd67865b3dd1f949bcaf0c0b4a0718916fd267
lydia chen <szupei21@gmail.com>
2024-07-18 23:20:07 +0800

    Refactor routes: Grouped units-related routes into units module

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
-const unitController = require('../controllers/unit-controller')
 const auth = require('../routes/modules/auth')
 const users = require('../routes/modules/users')
+const units = require('../routes/modules/units')
+
+const unitController = require('../controllers/unit-controller')
+
 const { authenticator } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/auth', auth)
 router.use('/users', users)
+router.use('/units', units)
+router.use('/', generalErrorHandler)
 
-router.get('/units/new', authenticator, unitController.unitsCreatePage)
-router.get('/units/delete', authenticator, unitController.unitsDeletePage)
-router.get('/units/:id/edit', authenticator, unitController.unitsEditPage)
-router.post('/units/new', authenticator, unitController.unitsCreate)
-router.delete('/units', authenticator, unitController.unitsDelete)
-router.get('/units/:id', authenticator, unitController.unitsId)
-router.put('/units/:id', authenticator, unitController.unitsEdit)
 router.get('/getAgencyDetail', unitController.getAgencyDetail)
 router.get('/', authenticator, unitController.unitsAll)
-router.use('/', generalErrorHandler)
 
 module.exports = router
---
+++ routes/modules/units.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+
+const unitController = require('../controllers/unit-controller')
+const { authenticator } = require('../middleware/auth')
+
+router.use('/', authenticator)
+
+router.get('/new', unitController.unitsCreatePage)
+router.get('/delete', unitController.unitsDeletePage)
+router.get('/:id/edit', unitController.unitsEditPage)
+router.post('/new', unitController.unitsCreate)
+router.delete('/', unitController.unitsDelete)
+router.get('/:id', unitController.unitsId)
+router.put('/:id', unitController.unitsEdit)
+
+module.exports = router
