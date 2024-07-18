82dbfa4bb77187344ee8803b748980709292f57d
lydia chen <szupei21@gmail.com>
2024-07-18 23:24:11 +0800

    refactor models/index.js

--- models/index.js
+++ models/index.js
@@ -10,23 +10,22 @@ const config = require(path.resolve(__dirname, '../config/config.json'))[env]
 const db = {}
 
 // belows are original setting
-// let sequelize
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config)
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config)
-// }
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: process.env.DB_PORT
-    // database: 'todo_sequelize2'
-  }
-)
+let sequelize
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config)
+} else {
+  // sequelize = new Sequelize(config.database, config.username, config.password, config)
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      port: process.env.DB_PORT
+    }
+  )
+}
 
 fs
   .readdirSync(__dirname)
